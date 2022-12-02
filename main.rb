2d04a72fe242efbdaf0f6392ed38e12a08b8eca1
Jakob Hillerström <j@kob.bz>
2022-12-02 09:55:32 +0000

    More sneakier

--- main.rb
+++ main.rb
@@ -2,8 +2,8 @@ part_1 = 0
 part_2 = 0
 File.readlines("input.txt").each do |l|
   part_1 += (l[2].ord - 87) + 
-             ([3, 6, 0][((-(l[0].ord - 65)) + (l[2].ord - 88)) % 3])
-  part_2 += ([1,2,3][((l[0].ord - 65 + 2) + (l[2].ord - 88)) % 3]) +
+             ((((((-(l[0].ord - 65)) + (l[2].ord - 88)) % 3) * 3) + 3) % 9)
+  part_2 += ((((l[0].ord - 65 + 2) + (l[2].ord - 88)) % 3) + 1) +
              ((l[2].ord - 88) * 3)
 end
 puts part_1
