e9519a8a0beee8fb2d25cda54d8c9f6d69c157f1
Jakob Hillerström <j@kob.bz>
2022-12-02 09:45:56 +0000

    Day 2

--- main.rb
+++ main.rb
@@ -1,1 +1,10 @@
-puts "Hello, World!"
+part_1 = 0
+part_2 = 0
+File.readlines("input.txt").each do |l|
+  part_1 += (l[2].ord - 87) + 
+             ([3, 6, 0][((-(l[0].ord - 65)) + (l[2].ord - 88)) % 3])
+  part_2 += ([1,2,3][((l[0].ord - 65 + 2) + (l[2].ord - 88)) % 3]) +
+             ((l[2].ord - 88) * 3)
+end
+puts part_1
+puts part_2
